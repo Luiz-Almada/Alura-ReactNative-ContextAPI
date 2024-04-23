7868ad54e7ec5ea6c24576f62a71853ec9812d8f
Luiz Almada <luizalmada@yahoo.com>
2024-04-23 08:06:18 -0300

    Parte 01

---
+++ src/contexts/GlobalContext.js
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+export const GlobalContext = createContext({});
+
+export function InfoProvider({ children }) {
+  const valor = 200;
+  const [nome, setNome] = useState("Almada")
+  return (
+    <GlobalContext.Provider
+      value={{
+        valor,
+        nome,
+        setNome
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+}
